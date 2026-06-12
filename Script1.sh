b192ae65b7c85734c6a90f6ebc3ad07673f95ea3
Rutuja Wavhal <rutujawavhal5@gmail.com>
2026-06-12 09:17:55 -0700

    Update learning message in Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,5 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-33 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Git"
-echo 'i am learning batch-32"
+echo 'i am learning jenkins CI/CD pipelines"
 
